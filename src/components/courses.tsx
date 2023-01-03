1e21b8f9270786c8705643d3d801481a0c48f6ee
Jomariel Gaitera <jojogaitera@gmail.com>
2023-01-03 17:42:35 +0800

    Add program view container

--- src/components/courses.tsx
+++ src/components/courses.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Program } from '../types'
 import { toast } from 'react-toastify'
 import { useCookies } from 'react-cookie'
 import useSWR from 'swr'
+import { ArrowLeftIcon } from '@heroicons/react/24/outline'
 
 export const Courses: FC<Record<string, never>> = () => {
   return (
@@ -39,23 +40,37 @@ export const CoursesProgramsCards: FC<Record<string, never>> = () => {
     error
   } = useSWR<Program[]>('/api/program/list', programFetcher)
 
+  const [view, setView] = useState<string>('')
+
   if (isLoading) return <p className='font-bold'>Loading..</p>
   if (error) return <p>An error occurs</p>
+
+  if (view) return <ProgramView view={view} close={() => setView('')} />
 
   return (
     <>
       {programs &&
+        !view &&
         programs.map((program) => (
-          <CoursesProgramsCard key={program.id} {...program} />
+          <CoursesProgramsCard
+            key={program.id}
+            program={program}
+            setProgram={() => setView(program.id)}
+          />
         ))}
     </>
   )
 }
 
-export const CoursesProgramsCard: FC<Program> = ({ color, alias, name }) => {
+export const CoursesProgramsCard: FC<{
+  setProgram: () => void
+  program: Program
+}> = ({ program, setProgram }) => {
+  const { color, alias, name } = program
   return (
     <div
       className={`bg-${color} w-[300px] h-[300px] md:w-[200px] md:h-[200px] lg:w-[300px] lg:h-[300px] rounded-lg p-4 text-white relative shadow md:cursor-pointer`}
+      onClick={setProgram}
     >
       <div className='w-full h-full grid place-items-center'>
         <div className='text-center flex flex-col gap-y-4 items-center'>
@@ -65,6 +80,28 @@ export const CoursesProgramsCard: FC<Program> = ({ color, alias, name }) => {
           <p className='md:text-sm lg:text-base'>{name}</p>
         </div>
       </div>
+    </div>
+  )
+}
+
+export const ProgramView: FC<{
+  view: string
+  close: () => void
+}> = ({ view: courseId, close }) => {
+  return (
+    <div>
+      <header className='pb-2 mb-4'>
+        <button onClick={close}>
+          <ArrowLeftIcon className='w-6 h-6' />
+        </button>
+      </header>
+      <h1>{courseId}</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
+        necessitatibus reiciendis nostrum ratione illo quam magnam, beatae,
+        repudiandae non laborum magni alias est soluta rerum placeat aspernatur.
+        Aperiam, atque laborum!
+      </p>
     </div>
   )
 }
